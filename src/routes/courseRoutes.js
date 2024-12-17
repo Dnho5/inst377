c472fb47f90adf086740df252f2a2279748e3e9d
Dnho5 <96706145+Dnho5@users.noreply.github.com>
2024-12-17 03:03:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/routes/courseRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getCourses, 
+    getCourseDetails
+    } = require ('../controllers/courseController');
+
+// fetch all courses 
+router.get('/', getCourses);
+
+//fetch  course by ID
+router.get('/:id', getCourseDetails);
+
+// //  fetch courses by a specific major
+// router.get('/majors/:majorId/courses', getCoursesByMajor);
+
+// //  sync courses from UMD API to the Supabase database
+// router.post('/sync', syncCourses);
+
+const { getPopularCourses } = require('../controllers/courseController');
+
+module.exports = router;
